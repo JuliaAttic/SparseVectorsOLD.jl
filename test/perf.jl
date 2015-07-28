4ce9197dd5ab3c4c9f253dd6286e74b21707e4bc
Dahua Lin <lindahua@gmail.com>
2015-07-28 23:35:29 +0800

    refactor test/perf.jl

--- test/perf.jl
+++ test/perf.jl
@@ -2,38 +2,37 @@
 
 using SparseVectors
 
-function bench_vadd(x::AbstractSparseVector, y::AbstractSparseVector, nr::Int)
-    println("Benchmark on sparse vector addition ...")
-    xmat = convert(SparseMatrixCSC, x)
-    ymat = convert(SparseMatrixCSC, y)
-    x + y
-    xmat + ymat
-    et1 = @elapsed for i = 1:nr
-        x + y
-    end
-    et2 = @elapsed for i = 1:nr
-        xmat + ymat
-    end
-    @printf("  sparse vector: %9.4fms (gain = %.4fx)\n", et1 * 1e3, et2 / et1)
-    @printf("  sparse matrix: %9.4fms\n", et2 * 1e3)
-end
+macro benchfun2(bfun, tfun, descr, xs, ys, nrtimes)
+    esc(quote
+        function $(bfun)(x::AbstractSparseVector, y::AbstractSparseVector, nr::Int)
+            println(string("Benchmark on ", $(string(descr)), " ..."))
+            xmat = convert(SparseMatrixCSC, x)
+            ymat = convert(SparseMatrixCSC, y)
 
-function bench_vmul(x::AbstractSparseVector, y::AbstractSparseVector, nr::Int)
-    println("Benchmark on sparse vector multiplication (elem-wise) ...")
-    xmat = convert(SparseMatrixCSC, x)
-    ymat = convert(SparseMatrixCSC, y)
-    x .* y
-    xmat .* ymat
-    et1 = @elapsed for i = 1:nr
-        x .* y
-    end
-    et2 = @elapsed for i = 1:nr
-        xmat .* ymat
-    end
-    @printf("  sparse vector: %9.4fms (gain = %.4fx)\n", et1 * 1e3, et2 / et1)
-    @printf("  sparse matrix: %9.4fms\n", et2 * 1e3)
-end
+            gc();
+            gc_enable(false)
 
+            # warming
+            $(tfun)(x, y)
+            $(tfun)(xmat, ymat)
+
+            # main profile
+            et1 = @elapsed for i = 1:nr
+                $(tfun)(x, y)
+            end
+            et2 = @elapsed for i = 1:nr
+                $(tfun)(xmat, ymat)
+            end
+
+            gc_enable(true)
+
+            # show result
+            @printf("  sparse vector: %9.4fms (gain = %.4fx)\n", et1 * 1e3, et2 / et1)
+            @printf("  sparse matrix: %9.4fms\n", et2 * 1e3)
+        end
+        $(bfun)($xs, $ys, $nrtimes)
+    end)
+end
 
 # data
 
@@ -41,8 +40,5 @@ n = 1_000_000
 x = sprand(n, 0.25)
 y = sprand(n, 0.25)
 
-gc();
-gc_enable(false);
-bench_vadd(x, y, 20)
-bench_vmul(x, y, 20)
-gc_enable(true);
+@benchfun2(bench_vadd, +, "sparse vector addition", x, y, 20)
+@benchfun2(bench_vmul, .*, "sparse vector multiplication", x, y, 20)
